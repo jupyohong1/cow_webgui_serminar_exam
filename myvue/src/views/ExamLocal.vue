8f041443082f3cd13958ebb1c47a417b2f8b9fa3
jupyohong <jupyohong@gmail.com>
2020-02-27 08:04:00 +0900

    단계별코드 추가

---
+++ myvue/src/views/ExamLocal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <counter name="LOCAL" :number="num.local" @changed="changedLocal"></counter>
+    <counter name="STORE" :number="num.store" @changed="changedStore"></counter>
+    <counter name="SERVER" :number="num.server" @changed="changedServer"></counter>
+  </div>
+</template>
+
+<script>
+import Counter from "../components/Counter";
+
+export default {
+  components: {
+    Counter
+  },
+  data() {
+    return {
+      num: {
+        local: 0,
+        store: 0,
+        server: 0
+      }
+    };
+  },
+  methods: {
+    changedLocal(btnType) {
+      if (btnType === "decrement") {
+        this.num.local--;
+      } else if (btnType === "increment") {
+        this.num.local++;
+      }
+    }
+  }
+};
+</script>
